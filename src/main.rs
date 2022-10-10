472c7fe4314f282968c12ae6962b03d673cce7ac
Sviatoslav Shkut <sgshkut@gmail.com>
2022-10-10 19:35:16 +0200

    Fixed errors

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,23 @@ fn main() {
         .read_line(&mut temperature)
         .expect("Failed to read input");
     let temperature: f32 = temperature
+        .trim()
         .parse()
         .expect("Cannot parse string {temperature}");
 
-    let result = if temperature_scale == "C" {
-        celsius_to_fahrenheit(temperature);
-    } else if temperature_scale == "F" {
-        fahrenheit_to_celsius(temperature);
+    let result: f32 = if temperature_scale == "C" {
+        celsius_to_fahrenheit(temperature)
+    } else {
+        fahrenheit_to_celsius(temperature)
     };
+
+    println!("Temperature is: {}", result);
 }
 
-fn fahrenheit_to_celsius(temperature: f32) {
-    5.0 / 9.0 * (temperature - 32.0);
+fn fahrenheit_to_celsius(temperature: f32) -> f32 {
+    5.0 / 9.0 * (temperature - 32.0)
 }
 
-fn celsius_to_fahrenheit(temperature: f32) {
-    1.8 * temperature + 32.0;
+fn celsius_to_fahrenheit(temperature: f32) -> f32 {
+    1.8 * temperature + 32.0
 }
